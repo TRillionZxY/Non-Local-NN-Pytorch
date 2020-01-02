da0be05a9008e9032cf85fdf36f7e0df149aebe8
Trillion Zhao <iezhaoxy@163.com>
2020-01-02 17:29:41 +0800

    personal setting

--- main.py
+++ main.py
@@ -16,15 +16,24 @@ import argparse
 from models.resnet2D import resnet2D56
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-parser.add_argument('--verbose', '-v', action='store_true', help='display progress bar')
-parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
-parser.add_argument('--nl', '-n', action='store_true', help='add non-local block')
+parser.add_argument('--lr', default=0.1, type=float,
+                    help='learning rate')
+parser.add_argument('--verbose', '-v', action='store_true',
+                    help='display progress bar')
+parser.add_argument('--resume', '-r', action='store_true',
+                    help='resume from checkpoint')
+parser.add_argument('--nl', '-n', action='store_true',
+                    help='add non-local block')
+parser.add_argument('-e', '--epoch', default=150,
+                    type=int, help='model train epoch')
+parser.add_argument('-g', '--gpu', default='2', type=str,
+                    help='GPU ID Select')
 args = parser.parse_args()
 
 if args.verbose:
     from utils import progress_bar
 
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
@@ -43,13 +52,18 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+trainset = torchvision.datasets.CIFAR10(
+    root='/mnt/ssd/zxy', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=256, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+testset = torchvision.datasets.CIFAR10(
+    root='/mnt/ssd/zxy', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=2)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
 
 # Model
 print('==> Building model..')
@@ -59,7 +73,6 @@ if args.nl:
 else:
     print("ResNet-56 without non-local block..")
     net = resnet2D56(non_local=False)
-
 
 
 net = net.to(device)
@@ -78,10 +91,14 @@ if args.resume:
     start_epoch = checkpoint['epoch']
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150], last_epoch=start_epoch - 1)
+optimizer = optim.SGD(net.parameters(), lr=args.lr,
+                      momentum=0.9, weight_decay=5e-4)
+lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+    optimizer, milestones=[100, 150], last_epoch=start_epoch - 1)
 
 # Training
+
+
 def train(epoch):
     print('\nEpoch: %d' % epoch)
     net.train()
@@ -103,11 +120,12 @@ def train(epoch):
 
         if args.verbose:
             progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+                         % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
     if not args.verbose:
         print('Loss: %.3f' % train_loss)
 
     return train_loss
+
 
 def test(epoch):
     global best_acc
@@ -125,10 +143,10 @@ def test(epoch):
             _, predicted = outputs.max(1)
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
-            
+
             if args.verbose:
                 progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                    % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+                             % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
     if not args.verbose:
         print('Loss: %.3f' % test_loss)
@@ -148,10 +166,11 @@ def test(epoch):
         best_acc = acc
     return test_loss
 
+
 tr_loss_list = []
 tst_loss_list = []
 
-for epoch in range(start_epoch, start_epoch+200):
+for epoch in range(start_epoch, start_epoch + args.epoch):
     train_l = train(epoch)
     lr_scheduler.step()
     test_l = test(epoch)
